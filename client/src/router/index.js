e74c3cc0f503f88e87cae2da5c14e9befcc327e8
Chamith Jayaweera <chamithlkml@gmail.com>
2023-12-05 00:12:58 +0000

    Adding form validation to merchant registration

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../components/Home.vue';
 import About from '../components/About.vue';
 import Contact from '../components/Contact.vue';
 import MerchantSignup from '../components/merchant_users/Signup.vue';
+import Register from '../components/merchant_users/Register.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
   { path: '/contact', component: Contact },
   { path: '/merchant_users/signup', component: MerchantSignup },
+  { path: '/register', component: Register },
 ];
 
 const router = createRouter({
