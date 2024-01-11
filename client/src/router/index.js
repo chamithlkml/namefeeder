94b9a92c1320317d70c2bd5d54bf2baad3d92725
Chamith Jayaweera <chamithlkml@gmail.com>
2024-01-11 10:56:27 +0000

    Fixed a typo on rouer/index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/components/Home.vue';
 import About from '@/components/About.vue';
 import Contact from '@/components/Contact.vue';
-import MerchantSignup from '@/components/merchant_users/Signup.vue';
+import MerchantSignup from '@/components/merchant_users/SignUp.vue';
 
 const routes = [
   { path: '/', component: Home },
