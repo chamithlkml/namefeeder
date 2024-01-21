7bb98cb2ab906509421fabee5b09e80f98687253
Chamith Jayaweera <chamithlkml@gmail.com>
2024-01-21 22:49:47 +0000

    Home page content added

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,5 +1,34 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <div class="container-fluid">
+      <div class="row mt-5 ms-5 mb-5 me-5">
+        <div class="row mt-5 ms-5 mb-5">
+          <div class="col-md-8 pt-5">
+            <div class="fs-3 mt-5">Add your products to Namefeeder</div>
+            <div class="fs-3 mt-5">Customers can order online</div>
+            <div class="fs-3 mt-5">Get paid instantly when our delivery agents pick the products from you</div>
+            <div class="fs-3 mt-5"><router-link to="/merchant_users/signup" tag="button" class="btn btn-primary">Sign Up as a Merchant</router-link></div>
+          </div>
+          <div class="col-md-4">
+            <img src="/src/assets/images/home-page-a.png" alt="" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row mt-5 ms-5 mb-5 me-5 pe-5">
+        <div class="row mt-5 ms-5 mb-5 me-5">
+          <div class="col-md-4 pe-5">
+            <img src="/src/assets/images/home-page-b.png" alt="" class="img-fluid">
+          </div>
+          <div class="col-md-8 pt-5 pe-5 text-end">
+            <div class="fs-3 mt-5">Purchase directly from manufacturers</div>
+            <div class="fs-3 mt-5">Buy quality products for a reasonable price</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
